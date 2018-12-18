e45c4c2c7e142220c193f680e96a39bfbedea864
Corvin Jaedicke <corvin.jaedicke@external.telekom.de>
2018-12-18 23:58:09 +0100

    split pieces in smaller parts, should increase convergence of the model

--- train_and_eval.py
+++ train_and_eval.py
@@ -99,6 +99,7 @@ def evaluate(X_data, Y_data):
         total_loss += loss.item()
         count += output.size(0)
 
+
     p, r, f1, a = prf_framewise(total_tp, total_fp, total_tn, total_fn)
     eval_loss = total_loss / count
     print("Validation/Test loss: {:.5f}".format(eval_loss))
